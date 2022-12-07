1462d0ff018d1305a8b094d9eda3318785fbb4ef
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-12-07 09:02:15 -0300

    fit function

--- nishikawa/fit.py
+++ nishikawa/fit.py
@@ -28,7 +28,7 @@ def find_nearest(arr, v):
 def xmax_in_sphere_map(l, chi):
     """Find the xmax value given l and chi."""
     maskl = find_nearest(DF.l, np.log10(l))
-    maskchi = find_nearest(DF.chi, np.log10(chi))
+    maskchi = find_nearest(DF.chi[maskl], np.log10(chi))
 
     idx = np.argwhere(np.asarray(maskl & maskchi))[0][0]
 
@@ -39,7 +39,7 @@ def grid_search(y_true, d, crates, dcoeffs, k0s):
     """Grid search for diffusion coefficients and rate constants."""
     min_mse = np.inf
 
-    for dcoeff, k0 in it.product(k0s, dcoeffs):
+    for dcoeff, k0 in it.product(dcoeffs, k0s):
 
         y_pred = [
             xmax_in_sphere_map(l(crate, d, dcoeff), chi(crate, k0, dcoeff))
@@ -54,3 +54,19 @@ def grid_search(y_true, d, crates, dcoeffs, k0s):
             best_coeffs = (dcoeff, k0)
 
     return best_coeffs, pred, min_mse
+
+
+def fit():
+    y_true = np.array([0.375, 0.365, 0.325, 0.295, 0.235]) / 0.4
+    d = 2 * np.sqrt(0.25 * 8.04e-6 / np.pi)
+    crates = [2.5, 5.0, 7.5, 12.5, 25.0]
+    dcoeffs = 10.**np.arange(-10, -6, 0.2)
+    k0s = 10.**np.arange(-9, -5, 0.2)
+    print(
+        y_true,
+        grid_search(y_true, d, crates, dcoeffs, k0s)
+    )
+
+
+if __name__ == "__main__":
+    fit()
