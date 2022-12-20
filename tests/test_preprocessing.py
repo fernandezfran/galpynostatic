c7f1325f94a916972a71a7de7b15ca85312c3dd7
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-12-20 15:23:10 -0300

    preprocessing test parametrized

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -20,6 +20,8 @@ import numpy as np
 
 import pandas as pd
 
+import pytest
+
 # ============================================================================
 # CONSTANTS
 # ============================================================================
@@ -33,17 +35,25 @@ TEST_DATA_PATH = pathlib.Path(
 # =============================================================================
 
 
-def test_get_discharge_capacities():
+@pytest.mark.parametrize(
+    ("ref", "dir_name", "file_names", "eq_pot"),
+    [
+        (  # nishikawa data
+            np.array(
+                [0.37869504, 0.3709768, 0.3157027, 0.2755689, 0.19977959]
+            ),
+            "LMNO",
+            ("1nA.csv", "2nA.csv", "3nA.csv", "5nA.csv", "10nA.csv"),
+            4.739,
+        )
+    ],
+)
+def test_get_discharge_capacities(ref, dir_name, file_names, eq_pot):
     """Test the get of discharge capacities."""
-    # reference xmaxs
-    ref = np.array([0.37869504, 0.3709768, 0.3157027, 0.2755689, 0.19977959])
-
-    # read experimental data
     dfs = [
-        pd.read_csv(TEST_DATA_PATH / "LMNO" / f"{i}nA.csv")
-        for i in (1, 2, 3, 5, 10)
+        pd.read_csv(TEST_DATA_PATH / dir_name / fname) for fname in file_names
     ]
 
-    xmaxs = galpynostatic.preprocessing.get_discharge_capacities(dfs, 4.739)
+    xmaxs = galpynostatic.preprocessing.get_discharge_capacities(dfs, eq_pot)
 
     np.testing.assert_array_almost_equal(xmaxs, ref, 6)
