ac9edff17a67d5de835646c9e6c79caec6cfeb1d
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-12-20 12:42:26 -0300

    reference to LICENSE in each of the main files

--- galpynostatic/__init__.py
+++ galpynostatic/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This file is part of galpynostatic
+#   https://github.com/fernandezfran/galpynostatic/
+# Copyright (c) 2022, Francisco Fernandez
+# License: MIT
+#   https://github.com/fernandezfran/galpynostatic/blob/master/LICENSE
+
 # =============================================================================
 # DOCS
 # =============================================================================
