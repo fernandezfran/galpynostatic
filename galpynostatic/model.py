6f041faf7b6ab57f325157e5da5aea3799c8811c
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-04-11 12:15:38 -0300

    order of authors

--- galpynostatic/model.py
+++ galpynostatic/model.py
@@ -88,7 +88,7 @@ class GalvanostaticRegressor(RegressorMixin):
 
     References
     ----------
-    .. [2] Fernandez, F., Gavilán-Arriazu, E.M., Visintín, A., Barraco, D.,
+    .. [2] Fernandez, F., Gavilán-Arriazu, E.M., Barraco, D., Visintín, A.,
        Ein-Eli, Y. and Leiva, E., 2023. Towards a fast-charging of LIBs
        electrode materials: a heuristic model based on galvanostatic
        simulations.
