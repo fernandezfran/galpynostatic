b9a86f0328d82d7aa5118dfe9d50da684e81d334
fernandezfran <fernandezfrancisco2195@gmail.com>
2023-07-05 14:54:05 -0300

    experimental instead of true

--- galpynostatic/model.py
+++ galpynostatic/model.py
@@ -263,10 +263,11 @@ class GalvanostaticRegressor(BaseEstimator, RegressorMixin):
 
         The coefficient of determination :math:`R^2` is defined as
         :math:`(1 - \frac{u}{v})`, where :math:`u` is the residual
-        sum of squares ``((y_true - y_pred)** 2).sum()`` and :math:`v`
-        is the total sum of squares ``((y_true - y_true.mean()) ** 2).sum()``.
-        The best possible score is 1.0 and it can be negative (because the
-        model can be arbitrarily worse).
+        sum of squares ``((y_experimental - y_pred)** 2).sum()`` and :math:`v`
+        is the total sum of squares
+        ``((y_experimental - y_experimental.mean()) ** 2).sum()``. The best
+        possible score is 1.0 and it can be negative (because the model can be
+        arbitrarily worse).
 
         Parameters
         ----------
