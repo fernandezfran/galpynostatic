5b181cc2192bac301eb93bbda06b9cdebf11fa10
fernandezfran <ffernandev@gmail.com>
2024-03-27 18:43:22 -0300

    cpp clang format style

---
+++ galpynostatic/lib/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of galpynostatic
+#   https://github.com/fernandezfran/galpynostatic/
+# Copyright (c) 2024, Francisco Fernandez, Maximilano Gavilán, Andres Ruderman
+# License: MIT
+#   https://github.com/fernandezfran/galpynostatic/blob/master/LICENSE
+
+# ============================================================================
+# DOCS
+# ============================================================================
+
+"""C++ files that are used in galpynostatic simulation module."""
